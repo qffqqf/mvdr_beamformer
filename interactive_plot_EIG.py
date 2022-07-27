136ca8c1db652fd41162b853e652e93d80320152
fei <sommeq7@gmail.com>
2022-07-28 00:53:36 +0200

    Improved  for mics on glasses legs

--- interactive_plot_EIG.py
+++ interactive_plot_EIG.py
@@ -6,13 +6,13 @@ from scipy import linalg
 def f(LOOK_DIRECTION, SIG_FREQ, N_ele):
     SOUND_SPEED = 343
     WAVE_LENGTH = SOUND_SPEED / SIG_FREQ
-    ANGLE = 30
+    ANGLE = 90
     Ny = 2
     Nx = N_ele-2*Ny
     Lx = 0.14
-    Ly = 0.1
-    alpha_left = np.deg2rad(np.arange(-90, LOOK_DIRECTION-ANGLE/2, 0.5).reshape([1, -1]))
-    alpha_right = np.deg2rad(np.arange(LOOK_DIRECTION+ANGLE/2, 90, 0.5).reshape([1, -1]))
+    Ly = 0.05
+    alpha_left = np.deg2rad(np.arange(-90, LOOK_DIRECTION-ANGLE/4, 0.5).reshape([1, -1]))
+    alpha_right = np.deg2rad(np.arange(LOOK_DIRECTION+ANGLE/4, 90, 0.5).reshape([1, -1]))
     alpha_out = np.concatenate( (alpha_left,alpha_right), axis=1)
     alpha_in = np.deg2rad(np.arange(LOOK_DIRECTION-ANGLE/2, LOOK_DIRECTION+ANGLE/2, 0.5).reshape([1, -1]))
     theta = np.deg2rad(np.arange(-90, 90, 0.1).reshape([1, -1]))
@@ -20,8 +20,8 @@ def f(LOOK_DIRECTION, SIG_FREQ, N_ele):
     mic_x_2 = np.ones([Ny,1])*Lx/2
     mic_x_3 = np.transpose(np.array([np.linspace(-Lx/2,Lx/2,Nx+2)]))
     mic_x_3 = mic_x_3[1:Nx+1] + 0.01*Lx*(np.random.rand(Nx,1)-0.5)/Nx
-    mic_y_1 = np.transpose(np.array([np.linspace(Ly/2,-Ly/2,Ny)]))
-    mic_y_2 = np.transpose(np.array([np.linspace(Ly/2,-Ly/2,Ny)]))
+    mic_y_1 = np.transpose(np.array([np.linspace(Ly/2,-Ly/2,Ny)])) - 0.2*Ly*np.random.rand(Ny,1)/Ny
+    mic_y_2 = np.transpose(np.array([np.linspace(Ly/2,-Ly/2,Ny)])) - 0.2*Ly*np.random.rand(Ny,1)/Ny
     mic_y_3 = np.ones([Nx,1])*Ly/2
     mic_x = np.concatenate( (np.concatenate((mic_x_1, mic_x_2), axis=0),mic_x_3), axis=0)
     mic_y = np.concatenate( (np.concatenate((mic_y_1, mic_y_2), axis=0),mic_y_3), axis=0)
